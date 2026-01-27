d01935902310180efd774056bc46576119b1377b
Lyle Cleveland <lyle.cleveland@proton.me>
2026-01-27 09:46:43 -0600

    test fix

--- settings/monitors.nix
+++ settings/monitors.nix
@@ -1,8 +1,8 @@
 { config, lib, ... }:
 {
   config.eiros.users.lcleveland.mangowc.settings.monitorrule = [
-    "eDP-1,normal,1,1920,1080,2560,1600,165"
-    "DP-10,normal,1,0,1080,1920,1080,60"
-    "DP-11,flipped-180,1,0,0,1920,1080,60"
+    "name:eDP-1,rr:0,scale:1,x:1920,y:1080,width:2560,height:1600,refresh:165,vrr:0"
+    "name:DP-10,rr:0,scale:1,x:0,y:1080,width:1920,height:1080,refresh:60,vrr:0"
+    "name:DP-11,rr:2,scale:1,x:0,y:0,width:1920,height:1080,refresh:60,vrr:0"
   ];
 }
